6497dee6c9658d95643fa2a847bc818973f7c9fe
Igaguri <igagurimk@gmail.com>
2019-03-30 12:46:05 +0900

    メインコマンド部分を関数に分離

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-use clap::{App, Arg};
-use failure::{Error, format_err};
+use clap::{App, Arg, ArgMatches};
+use failure::{format_err, Error};
 
 use crate::recipe::load_recipes;
 use crate::solver::Solver;
 use crate::target::load_target_settings;
 
 mod recipe;
-mod target;
 mod solver;
 mod sub;
+mod target;
 
-use sub::{SubCmd, RecipeCheck};
+use sub::{RecipeCheck, SubCmd};
 
 type Result<T> = std::result::Result<T, Error>;
 
 fn main() {
     match w_main() {
-        Ok(()) => {},
+        Ok(()) => {}
         Err(err) => {
             eprintln!("{}", err);
             std::process::exit(1);
@@ -33,45 +33,40 @@ fn w_main() -> Result<()> {
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .subcommand(recipe_check.command_args())
-        .arg(Arg::with_name("mult")
-            .long("mult")
-            .short("m")
-            .default_value("1.0")
-        )
-        .arg(Arg::with_name("all-merged")
-            .long("all-merged")
-            .short("M")
-        )
-        .arg(Arg::with_name("never-merged")
-            .long("never-merged")
-            .short("N")
-            .multiple(true)
-            .number_of_values(1)
-            .takes_value(true)
-        )
-        .arg(Arg::with_name("no-beacon")
-            .long("no-beacon")
-        )
-        .arg(Arg::with_name("no-speed")
-            .long("no-speed")
-        )
-        .arg(Arg::with_name("no-prod")
-            .long("no-prod")
-        )
         .arg(
-            Arg::with_name("target-file")
+            Arg::with_name("mult")
+                .long("mult")
+                .short("m")
+                .default_value("1.0"),
         )
+        .arg(Arg::with_name("all-merged").long("all-merged").short("M"))
+        .arg(
+            Arg::with_name("never-merged")
+                .long("never-merged")
+                .short("N")
+                .multiple(true)
+                .number_of_values(1)
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("no-beacon").long("no-beacon"))
+        .arg(Arg::with_name("no-speed").long("no-speed"))
+        .arg(Arg::with_name("no-prod").long("no-prod"))
+        .arg(Arg::with_name("target-file"))
         .get_matches_safe()?;
 
     //// Sub commands
-    
+
     if let Some(m) = matches.subcommand_matches(recipe_check.name()) {
         return recipe_check.exec(m);
     }
-    
-    //// Main command
 
-    let target_settings_file_name = matches.value_of("target-file").ok_or_else(|| {format_err!("target file required.")})?;    
+    main_cmd(matches)
+}
+
+fn main_cmd(matches: ArgMatches) -> Result<()> {
+    let target_settings_file_name = matches
+        .value_of("target-file")
+        .ok_or_else(|| format_err!("target file required."))?;
     let mult = matches.value_of("mult").unwrap().parse::<f64>()?;
 
     let mut target_settings = load_target_settings(&target_settings_file_name);
@@ -82,7 +77,11 @@ fn w_main() -> Result<()> {
         .speed_module(!matches.is_present("no-speed"))
         .productivity_module(!matches.is_present("no-prod"));
 
-    let mut solver = Solver::new(load_recipes("./data/recipes")?, &target_settings, processer_choice);
+    let mut solver = Solver::new(
+        load_recipes("./data/recipes")?,
+        &target_settings,
+        processer_choice,
+    );
 
     solver.all_merged(matches.is_present("all-merged"));
     if let Some(never_merged) = matches.values_of("never-merged") {
